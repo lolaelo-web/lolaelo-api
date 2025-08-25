259494d3833b9aad7a0f3079bd90b8723a6ff4f4
lolaelo-web <palatulan@gmail.com>
2025-08-25 12:12:32 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get('/healthz', (_req, res) => res.json({ ok: true }));
-app.get('/', (_req, res) => res.send('Lolaelo API is running.'));
+app.get('/healthz', (_req: Request, res: Response) => res.json({ ok: true }));
+app.get('/', (_req: Request, res: Response) => res.send('Lolaelo API is running.'));
 
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, () => console.log(`API listening on :${port}`));
